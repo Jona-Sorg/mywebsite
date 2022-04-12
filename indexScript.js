2c6184b7d40b353072277851f2da55555f30caae
Jona-Sorg <jona.sorg@me.com>
2022-04-12 17:59:55 +0200

    Update Repository

--- indexScript.js
+++ indexScript.js
@@ -2,4 +2,20 @@ const width = window.innerWidth;
 const landscape = document.getElementById("landscape");
 const pb = document.getElementById("pb");
 
+const navbar = document.getElementsByName("navbar")[0];
+
+window.onscroll = function() {scroll()};
+
+var sticky = navbar.offsetTop;
+
+function scroll(){
+    console.log(navbar);
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+
+
 
